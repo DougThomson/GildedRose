093906892c1754407e5d779ccfc7d955b9436f7d
Doug Thomson <dougthomson1970@live.co.uk>
2015-10-14 17:02:23 +0100

    Extracted Quality calculation into its own class

---
+++ GildedRose.Lib/Item.cs
@@ -0,0 +1,11 @@
+namespace GildedRose.Lib
+{
+    public class Item
+    {
+        public string Name { get; set; }
+
+        public int SellIn { get; set; }
+
+        public int Quality { get; set; }
+    }
+}
