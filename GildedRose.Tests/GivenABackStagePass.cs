87c9879f7d17174d1e49d6eb768a6ae179254418
Doug Thomson <dougthomson1970@live.co.uk>
2015-10-19 06:10:54 +0100

    Added other test cases for Backstage tickets having Selling between 10 and 6. Also small refatoring

--- GildedRose.Tests/GivenABackStagePass.cs
+++ GildedRose.Tests/GivenABackStagePass.cs
@@ -1,29 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using GildedRose.Lib;
 using GildedRose.Tests;
 using NUnit.Framework;
 
+// ReSharper disable once CheckNamespace
 namespace GivenABackStagePass
 {
     [TestFixture]
+    // ReSharper disable once InconsistentNaming
     public class WhenThereAreBetween_10_and_6_days_Sellin_left
     {
         [Test]
         [TestCase(10)]
+        [TestCase(9)]
+        [TestCase(8)]
+        [TestCase(7)]
+        [TestCase(6)]
         public void Then_QualityIncreasesBy2_EachDay(int initialSellinValue)
         {
             IList<Item> items = TestResources.GetBackStagePassesHavingSellinOf(initialSellinValue);
-            int originalQuality = GildedTestHelper.GetQuality(items, TestResources.BackstagePassesToATafkal80EtcConcert);
+            int originalQuality = GetQualityOfBackstagePass(items);
             QualityCalculator qualityCalculator = new QualityCalculator(items);
 
             qualityCalculator.UpdateQuality();
-            int updatedQuality = GildedTestHelper.GetQuality(items, TestResources.BackstagePassesToATafkal80EtcConcert);
+            int updatedQuality = GetQualityOfBackstagePass(items);
             int qualityDifference = updatedQuality - originalQuality;
 
             Assert.That(qualityDifference, Is.EqualTo(2));
         }
-    }}
+
+        private static int GetQualityOfBackstagePass(IList<Item> items)
+        {
+            int originalQuality = GildedTestHelper.GetQuality(items, TestResources.BackstagePassesToATafkal80EtcConcert);
+            return originalQuality;
+        }
+    }
+}
